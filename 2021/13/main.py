a8b5f6c7a62fb9601257fab3ea1322b1824c01d4
Tero Hongisto <tehongis@gmail.com>
2021-12-14 18:42:41 +0200

    Update main.py

--- 2021/13/main.py
+++ 2021/13/main.py
@@ -6,28 +6,79 @@ f.close()
 
 dotlist = []
 
+def foldbyx(array,foldpoint):
+    new_array = []
+    for line in array:
+        halfa = line[:foldpoint]
+        halfb = line[foldpoint:]
+        halfb = halfb[::-1]
+        print(halfa,halfb)
+        for count in range(len(halfa)):
+            if halfb[count]=='#':
+                halfa[count]='#'
+        new_array.append(halfa)
+    return new_array
+
+def foldbyy(array,foldpoint):
+    new_array = []
+    for count in range(foldpoint):
+        linea = array[count]
+        lineb = array[foldpoint+foldpoint-count]
+        for count in range(len(linea)):
+            if lineb[count]=='#':
+                linea[count]='#'
+        new_array.append(linea)
+    return new_array
+
+def printdots(array):
+    for line in array:
+        for char in line:
+            print(char,end='')
+        print()
+
+def countdots(array):
+    count = 0
+    for line in array:
+        for char in line:
+            if char == '.':
+                count = count + 1
+    return count
+
+
+
 maxx=0
 maxy=0
+dotlist=[]
+foldlist=[]
+readmode = 'dots'
 for line in filedata:
     line = line.strip()
     if line == '':
-        break
-    x,y = line.split(',')
-    x=int(x)
-    y=int(y)
+        pass
+    elif line[0].isnumeric():
+        x,y = line.split(',')
+        x=int(x)
+        y=int(y)
+        if maxx < x:
+            maxx = x
+        if maxy < y:
+            maxy = y
+        dotlist.append([x,y])
+    else:
+        line = line.strip()
+        line = line [11:]
+        axis,fold = line.split('=')
+        foldlist.append([axis,fold])
 
-    if x > maxx:
-        maxx=x
-    if y > maxy:
-        maxy=y
-    dotlist.append([x,y])
-
-print(maxx,maxy)
-dots = [['.' for c in range(15)] for r in range(15)]
+dots = [['.' for c in range(maxx+1)] for r in range(maxy+1)]
 for x,y in dotlist:
     dots[y][x]='#'
 
-for line in dots:
-    for char in line:
-        print(char,end='')
-    print()
+printdots(dots)
+for axis,fold in foldlist:
+    if axis == 'x':
+        dots = foldbyx(dots,int(fold))
+    if axis == 'y':
+        dots = foldbyy(dots,int(fold))
+    printdots(dots)
+    print(countdots(dots))
