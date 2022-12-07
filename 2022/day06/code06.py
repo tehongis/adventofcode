b8177e8ba72bb647757516d6300ecef5b22025fe
Tero Hongisto <tehongis@gmail.com>
2022-12-07 21:03:09 +0200

    day06 done

--- 2022/day06/code06.py
+++ 2022/day06/code06.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-""" https://adventofcode.com/2022/day/1 """
+""" https://adventofcode.com/2022/day/6 """
 
 import time
 import os
@@ -11,6 +11,27 @@ f = open('input.txt','r')
 data = f.readlines()
 f.close()
 
+jono = "                       "
+checksize = 4
+#checksize = 14 part 2
 
-for line in data:
-    print(line)
+def checkBuffer(jono):
+    char_set = [False] * 128
+    for c in jono:
+        val = ord(c)
+        if char_set[val]:
+            return False
+        char_set[val] = True
+    return True
+
+count = 0
+for row in data:
+    for merkki in row:
+        if merkki == "\n":
+            break
+        jono = jono[1:checksize] +merkki
+        count += 1
+        if count >= checksize:
+            if checkBuffer(jono):
+                print(jono,count)
+                break
