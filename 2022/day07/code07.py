e5b14bfb7aa1be96e396dd26f4fc66c879891386
Tero Hongisto <tehongis@gmail.com>
2022-12-09 10:02:06 +0200

    Update code07.py

--- 2022/day07/code07.py
+++ 2022/day07/code07.py
@@ -11,5 +11,59 @@ f = open('input.txt','r')
 data = f.readlines()
 f.close()
 
-for line in data:
-    print(line)
+#$ cd stcfmz
+#$ ls
+#115637 lcgv
+
+linecount=0
+
+
+def nextLine():
+    global linecount
+    if linecount > len(data):
+        return ""
+    line=data[linecount]
+    if len(line) == 0:
+        return (line)
+    linecount += 1 
+    return line.strip()
+
+
+def getFiles():
+    global linecount
+    output = []
+    while True:
+        line = nextLine()
+        if line[0] == '$':
+            linecount -= 1
+            return output
+        output.append(line)
+
+
+filesystem = []
+while linecount < len(data):
+    line = nextLine()
+    if len(line) == 0:
+        break
+    if line[0] == '$':
+        command = line[2:]
+        if command[0:2] == 'ls':
+            print(command)
+            result = getFiles()
+            for line in result:
+                print(line)
+
+        if command[0:2] == 'cd':
+            target = command[command.find(' ')+1:]
+            if target == '..':
+                print('cd up')
+            else:
+                print('cd to:', target)
+
+#    if linecount > 20:
+#        break
+
+
+#    line=line.strip()
+#    if line[0]=='$':
+#        parsecommand(line)   
